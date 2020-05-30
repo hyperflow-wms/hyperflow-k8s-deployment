0d6d202b569a4a9b7eaeb28ce787466725b79b1d
Bartosz Balis <balis@agh.edu.pl>
2020-05-30 16:13:10 +0000

    Add two useful functions

--- helper_functions.sh
+++ helper_functions.sh
@@ -4,8 +4,14 @@ hf-pod-name() {
 hf-exec() {
   kubectl exec -it $(hf-pod-name) -c hyperflow sh 
 }
+hf-tools-exec() {
+  kubectl exec -it $(hf-pod-name) -c hflow-tools bash
+}
 hf-logs() {
   kubectl logs $(hf-pod-name) -c hyperflow -f 
+}
+hf-detect-zombies() {
+  kubectl exec -it $(hf-pod-name) -c hyperflow -- find /work_dir/logs-hf -name \*log | xargs -I{} sh -c "echo $0 `grep Warning $0 | wc -l`" "{}" | grep "g 0"
 }
 
 echo "Hyperflow helper functions:"
